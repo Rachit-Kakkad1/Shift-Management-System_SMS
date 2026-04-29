84a043bf18125ff93fb4e1b55c9fcf66e878c344
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-29 13:30:00 +0530

    feat(frontend): add UserCalendar component

--- frontend/src/components/Calendars/UserCalendar.jsx
+++ frontend/src/components/Calendars/UserCalendar.jsx
@@ -4,6 +4,40 @@ import { viewWeek, viewDay, viewMonthGrid } from "@schedule-x/calendar";
 
 const UserCalendar = ({ events }) => {
   const calendar = useCalendarApp({
+    calendars: {
+      sozialarbeiter: {
+        colorName: "sozialarbeiter",
+        lightColors: {
+          main: "#f97316",
+          container: "#fff7ed",
+          onContainer: "#7c2d12",
+        },
+      },
+      sozialbetreuer: {
+        colorName: "sozialbetreuer",
+        lightColors: {
+          main: "#1c1917",
+          container: "#f5f5f4",
+          onContainer: "#1c1917",
+        },
+      },
+      sozialbetreuerhelfer: {
+        colorName: "sozialbetreuerhelfer",
+        lightColors: {
+          main: "#f59e0b",
+          container: "#fffbeb",
+          onContainer: "#78350f",
+        },
+      },
+      partTime: {
+        colorName: "partTime",
+        lightColors: {
+          main: "#10b981",
+          container: "#ecfdf5",
+          onContainer: "#064e3b",
+        },
+      },
+    },
     locale: "en-US",
     defaultView: viewMonthGrid.name,
     skipValidation: true,
@@ -17,9 +51,11 @@ const UserCalendar = ({ events }) => {
     showWeekNumbers: true,
     events: events,
     views: [viewWeek, viewDay, viewMonthGrid],
+    isDark: false,
   });
+
   return (
-    <div>
+    <div className="h-[700px] w-full">
       <ScheduleXCalendar calendarApp={calendar} />
     </div>
   );
