1dac16258bbd26b847ed32a6aae4e5a1e05677ab
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-29 13:00:00 +0530

    feat(frontend): add ShiftCalendar component

--- frontend/src/components/Calendars/ShiftCalendar.jsx
+++ frontend/src/components/Calendars/ShiftCalendar.jsx
@@ -12,42 +12,35 @@ const ShiftCalendar = ({
   const calendar = useCalendarApp({
     calendars: {
       sozialarbeiter: {
-        colorName: "leisure",
+        colorName: "sozialarbeiter",
         lightColors: {
-          main: "#1c7df9",
-          container: "#d2e7ff",
-          onContainer: "#002859",
-        },
-        darkColors: {
-          main: "#c0dfff",
-          onContainer: "#dee6ff",
-          container: "#426aa2",
+          main: "#f97316",
+          container: "#fff7ed",
+          onContainer: "#7c2d12",
         },
       },
       sozialbetreuer: {
-        colorName: "work",
+        colorName: "sozialbetreuer",
         lightColors: {
-          main: "#f91c45",
-          container: "#ffd2dc",
-          onContainer: "#59000d",
-        },
-        darkColors: {
-          main: "#ffc0cc",
-          onContainer: "#ffdee6",
-          container: "#a24258",
+          main: "#1c1917",
+          container: "#f5f5f4",
+          onContainer: "#1c1917",
         },
       },
       sozialbetreuerhelfer: {
-        colorName: "personal",
+        colorName: "sozialbetreuerhelfer",
         lightColors: {
-          main: "#efb100",
-          container: "#fff5aa",
-          onContainer: "#594800",
+          main: "#f59e0b",
+          container: "#fffbeb",
+          onContainer: "#78350f",
         },
-        darkColors: {
-          main: "#fff5c0",
-          onContainer: "#fff5de",
-          container: "#a29742",
+      },
+      partTime: {
+        colorName: "partTime",
+        lightColors: {
+          main: "#10b981",
+          container: "#ecfdf5",
+          onContainer: "#064e3b",
         },
       },
     },
@@ -68,11 +61,13 @@ const ShiftCalendar = ({
       },
     },
     showWeekNumbers: true,
-    plugins: [createEventModalPlugin(), createDragAndDropPlugin()],
+    plugins: [createEventModalPlugin()],
     events: events,
+    isDark: false,
   });
+
   return (
-    <div>
+    <div className="h-[800px] w-full">
       <ScheduleXCalendar calendarApp={calendar} />
     </div>
   );
